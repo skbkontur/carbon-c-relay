1dc633a16455a23a8aaac0f7ef5fb2f81835cc02
msaf1980 <msaf1980@gmail.com>
2020-04-02 10:09:58 +0500

    tests: server shutdown timeout

--- server_test.c
+++ server_test.c
@@ -30,6 +30,11 @@
 #include <errno.h>
 #include <libgen.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include "queue.h"
 #include "consistent-hash.h"
 #include "router.h"
@@ -49,6 +54,7 @@ relaylog(enum logdst dest, const char *fmt, ...)
 router *router_new(void);
 char *router_validate_address(router *rtr, char **retip, unsigned short *retport, void **retsaddr, void **rethint, char *ip, con_proto proto);
 queue *server_queue(server *s);
+void server_set_port(server *s, unsigned short port);
 
 #include "minunit.h"
 
@@ -77,6 +83,8 @@ MU_TEST_STEP(test_server_send, char*) {
 	char *firstspace;
 	router *rtr;
 	cluster *cl;
+	size_t metrics = 0;
+	size_t dropped = 0;
 
 	strcpy(relay_hostname, "relay");
 
@@ -104,17 +112,97 @@ MU_TEST_STEP(test_server_send, char*) {
 	cl = router_cluster(rtr, "test");
 	if (cl == NULL) {
 		mu_fail_step("cluster test not found", param);
-	} else {
-		size_t metrics = 0;
-		size_t dropped = 0;
-
-		for (i = 0; i < cl->members.anyof->count; i++) {
-			metrics += queue_len(server_queue(cl->members.anyof->servers[i]));
-			dropped += server_get_dropped(cl->members.anyof->servers[i]);
-		}
-		mu_assert_step(metrics == send_metrics, "server_send metrics count", param);
-		mu_assert_step(dropped == 0, "server_send drop", param);
+		return;
 	}
+
+	for (i = 0; i < cl->members.anyof->count; i++) {
+		metrics += queue_len(server_queue(cl->members.anyof->servers[i]));
+		dropped += server_get_dropped(cl->members.anyof->servers[i]);
+	}
+	mu_assert_step(metrics == send_metrics, "server_send metrics count", param);
+	mu_assert_step(dropped == 0, "server_send drop", param);
+
+	router_free(rtr);
+}
+
+MU_TEST_STEP(test_server_shutdown_timeout, char*) {
+	size_t destlen = 0, len = 0, blackholed = 0;
+	char config[280];
+	int queuesize = 100;
+	int batchsize = 10;
+	size_t send_metrics = 2 * queuesize - 4 * batchsize;
+	int i, j;
+	destination dests[DESTS_SIZE];
+	char *metric;
+	char *firstspace;
+	router *rtr;
+	cluster *cl;
+	time_t start, stop;
+	unsigned long long elapsed;
+	size_t metrics = 0;
+	size_t dropped = 0;
+	int socks[32];
+
+	shutdown_timeout = 20;
+
+	strcpy(relay_hostname, "relay");
+
+	snprintf(config, sizeof(config), "%s/%s.conf", testdir, param);
+	rtr = router_readconfig(NULL, config, 1, queuesize,
+					batchsize, 1, 600, 0, 2003);
+	if (rtr == NULL) {
+		mu_fail_step("router_readconfig failed", param);
+		return;
+	}
+
+	cl = router_cluster(rtr, "test");
+	if (cl == NULL) {
+		mu_fail_step("cluster test not found", param);
+		return;
+	}
+
+	for (i = 0; i < cl->members.anyof->count; i++) {
+		socklen_t addr_len;
+		socks[i] = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+		struct sockaddr_in addr;
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(0); /* исправлено */
+		addr.sin_addr.s_addr = htonl(INADDR_ANY);
+		if (bind(socks[i], (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+			mu_fail_step("bind", param);
+		}
+		getsockname(socks[i], (struct sockaddr*)&addr, &addr_len);
+		server_set_port(cl->members.anyof->servers[i], addr.sin_port);
+	}
+
+	for (i = 0; i < cl->members.anyof->count; i++) {
+		server_start(cl->members.anyof->servers[i]);
+	}
+	for (i = 0; i < send_metrics; i++) {
+		metric = strdup(m);
+		firstspace = metric + strlen(metric);
+		if (router_route(rtr, dests, &len, DESTS_SIZE, "127.0.0.1", metric, firstspace, 1) == 0) {
+			for (j = 0; j < len; j++) {
+				server_send(dests[j].dest, dests[j].metric, 0);
+			}
+			destlen += len;
+		} else {
+			blackholed++;
+		}
+	}
+	mu_assert_step(blackholed == 0, "router_route blackholed", param);
+	start = time(NULL);
+	router_shutdown(rtr);
+	stop = time(NULL);
+	elapsed = stop - start;
+	mu_assert_step(elapsed < shutdown_timeout + 20, "router_shutdown timeout", param);
+
+	for (i = 0; i < cl->members.anyof->count; i++) {
+		metrics += queue_len(server_queue(cl->members.anyof->servers[i]));
+		dropped += server_get_dropped(cl->members.anyof->servers[i]);
+	}
+	mu_assert_step(metrics == send_metrics, "server_send metrics count", param);
+
 	router_free(rtr);
 }
 
@@ -124,6 +212,7 @@ MU_TEST_SUITE(server_send_suite) {
 
 	for (i = 0; i < n; i++) {
 		MU_RUN_TEST_STEP(test_server_send, steps[i]);
+		MU_RUN_TEST_STEP(test_server_shutdown_timeout, steps[i]);
 	}
 }
 
